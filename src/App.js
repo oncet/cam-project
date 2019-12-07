1208f6d121f26ffd75e503c63121cd3a81116ba6
oncet <camilorivera86@gmail.com>
2019-12-07 17:21:34 -0300

    Trying out face-api.js with images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect } from 'react';
 import './App.css';
 
-function App() {
+import * as faceapi from 'face-api.js';
+
+const App = () => {
+  const canvasRef = useRef();
+  useEffect(() => {
+    const loadModels = async () => {
+      await faceapi.nets.ssdMobilenetv1.loadFromUri('/models');
+    };
+    loadModels();
+  }, []);
+
+  const loadImage = event => {
+    const file = event.target.files[0];
+    const image = new Image();
+    image.onload = async () => {
+      const faces = await faceapi.detectAllFaces(image);
+      const canvas = canvasRef.current;
+      canvas.width = image.width;
+      canvas.height = image.height;
+      canvas.getContext('2d').drawImage(image, 0, 0);
+      faceapi.draw.drawDetections(canvas, faces);
+    }; 
+    const reader = new FileReader();
+    reader.onload = event => {
+      image.src = event.target.result;
+    };
+    reader.readAsDataURL(file);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input type="file" onChange={loadImage} />
+      </form>
+      <canvas ref={canvasRef}></canvas>
     </div>
   );
-}
+};
 
 export default App;
