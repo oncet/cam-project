fe90586badfec3396c95bc72dc5e81908ee711ff
oncet <camilorivera86@gmail.com>
2019-12-21 15:51:25 -0300

    Improved performace Use tinyFaceDetector and cap to 30 FPS

--- src/App.js
+++ src/App.js
@@ -9,24 +9,31 @@ const App = () => {
 
   useEffect(() => {
     const loadModels = async () => {
-      await faceapi.nets.ssdMobilenetv1.loadFromUri('/models');
+      await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
     };
     loadModels();
   }, []);
 
   useEffect(() => {
     const context = canvasRef.current.getContext('2d');
+    const detectorOptions = new faceapi.TinyFaceDetectorOptions({ inputSize: 128 });
     const stream = async () => {
       const stream = await navigator.mediaDevices.getUserMedia({
         'video': true
       });
+
       videoRef.current.srcObject = stream;
-      videoRef.current.addEventListener('progress', async () => {
-        context.drawImage(videoRef.current, 0, 0);
-        const faces = await faceapi.detectAllFaces(videoRef.current);
-        faceapi.draw.drawDetections(canvasRef.current, faces);
-      });
+
+      videoRef.current.addEventListener('play', () => {
+        (async function loop() {
+          const faces = await faceapi.detectSingleFace(videoRef.current, detectorOptions);
+          context.drawImage(videoRef.current, 0, 0);
+          faces && faceapi.draw.drawDetections(canvasRef.current, faces);
+          setTimeout(loop, 1000 / 300 ); // Drawing at 30 FPS
+        })();
+      }, 0);
     };
+
     // TODO: Prevent starting stream before models are loaded
     window.setTimeout(stream, 5000);
   }, []);
