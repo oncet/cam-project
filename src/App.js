192d0b87fe65f0c1146eebaa37ce02855f8cc5af
oncet <camilorivera86@gmail.com>
2020-01-30 22:16:56 -0300

    Rendered webcam video inside three.js

--- src/App.js
+++ src/App.js
@@ -1,62 +1,64 @@
 import React, { useRef, useEffect } from 'react';
 import './App.css';
 
-import * as faceapi from 'face-api.js';
+import * as THREE from 'three';
+
+const stream = async (videoRef, canvasContainerRef) => {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    'video': true
+  });
+
+  videoRef.current.srcObject = stream;
+
+  videoRef.current.onloadedmetadata = () => {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    canvasContainerRef.current.appendChild( renderer.domElement );
+
+    var backgroundTexture = new THREE.VideoTexture( videoRef.current );
+    var backgroundMaterial = new THREE.MeshBasicMaterial( { map: backgroundTexture } );
+    var backgroundGeometry = new THREE.BoxGeometry(videoRef.current.videoWidth / 100, videoRef.current.videoHeight / 100);
+    var background = new THREE.Mesh( backgroundGeometry, backgroundMaterial );
+
+    scene.add( background );
+
+    var geometry = new THREE.BoxGeometry();
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    cube.position.z = 5;
+
+    scene.add( cube );
+
+    camera.position.z = 10;
+
+    var animate = function () {
+      requestAnimationFrame( animate );
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render( scene, camera );
+    };
+
+    animate();
+  };
+};
 
 const App = () => {
-  const canvasRef = useRef();
+  const canvasContainerRef = useRef();
   const videoRef = useRef();
 
   useEffect(() => {
-    const loadModels = async () => {
-      await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
-      await faceapi.nets.faceLandmark68TinyNet.loadFromUri('/models');
-    };
-    loadModels();
-  }, []);
-
-  useEffect(() => {
-    const context = canvasRef.current.getContext('2d');
-
-    // Recommended setting for webcams
-    // See https://github.com/justadudewhohacks/face-api.js/#tinyfacedetectoroptions
-    const detectorOptions = new faceapi.TinyFaceDetectorOptions({ inputSize: 320 });
-
-    videoRef.current.onloadedmetadata = () => {
-      canvasRef.current.width = videoRef.current.videoWidth;
-      canvasRef.current.height = videoRef.current.videoHeight;
-    };
-
-    const stream = async () => {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        'video': true
-      });
-
-      videoRef.current.srcObject = stream;
-
-      videoRef.current.addEventListener('play', () => {
-        (async function loop() {
-          const detections = await faceapi.detectSingleFace(videoRef.current, detectorOptions).withFaceLandmarks(true);
-          context.drawImage(videoRef.current, 0, 0);
-          if (detections) {
-            faceapi.draw.drawDetections(canvasRef.current, detections);
-            faceapi.draw.drawFaceLandmarks(canvasRef.current, detections);
-          }
-
-          // Draw at 30 FPS
-          setTimeout(loop, 1000 / 300 );
-        })();
-      }, 0);
-    };
-
-    // TODO Prevent starting stream before models are loaded
-    window.setTimeout(stream, 5000);
+    stream(videoRef, canvasContainerRef);
   }, []);
 
   return (
     <div>
-      <canvas ref={canvasRef}></canvas>
-      <video autoPlay ref={videoRef}></video>
+      <div className="canvasContainer" ref={canvasContainerRef} />
+      <video autoPlay ref={videoRef} />
     </div>
   );
 };
