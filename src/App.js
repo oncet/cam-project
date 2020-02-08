ff53afd5e113267f2c9728718f617c2ab1b823a6
oncet <camilorivera86@gmail.com>
2020-02-07 22:27:43 -0300

    Use const instead of var

--- src/App.js
+++ src/App.js
@@ -9,25 +9,25 @@ const App = () => {
   useEffect(() => {
 
     videoRef.current.onloadedmetadata = () => {
-      var scene = new THREE.Scene();
-      var camera = new THREE.PerspectiveCamera( 50, videoRef.current.videoWidth/videoRef.current.videoHeight, 0.1, 1000 );
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera( 50, videoRef.current.videoWidth/videoRef.current.videoHeight, 0.1, 1000 );
   
-      var renderer = new THREE.WebGLRenderer();
+      const renderer = new THREE.WebGLRenderer();
       renderer.setSize( videoRef.current.videoWidth, videoRef.current.videoHeight );
       canvasContainerRef.current.appendChild( renderer.domElement );
   
-      var backgroundTexture = new THREE.VideoTexture( videoRef.current );
-      var backgroundMaterial = new THREE.MeshBasicMaterial( { map: backgroundTexture } );
-      var backgroundGeometry = new THREE.BoxGeometry(videoRef.current.videoWidth, videoRef.current.videoHeight);
-      var background = new THREE.Mesh( backgroundGeometry, backgroundMaterial );
+      const backgroundTexture = new THREE.VideoTexture( videoRef.current );
+      const backgroundMaterial = new THREE.MeshBasicMaterial( { map: backgroundTexture } );
+      const backgroundGeometry = new THREE.BoxGeometry(videoRef.current.videoWidth, videoRef.current.videoHeight);
+      const background = new THREE.Mesh( backgroundGeometry, backgroundMaterial );
   
       background.position.z = -500; 
   
       scene.add( background );
   
-      var geometry = new THREE.BoxGeometry();
-      var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-      var cube = new THREE.Mesh( geometry, material );
+      const geometry = new THREE.BoxGeometry();
+      const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+      const cube = new THREE.Mesh( geometry, material );
       cube.position.z = 5;
   
       scene.add( cube );
@@ -42,13 +42,17 @@ const App = () => {
       const detect = async (detectorOptions) => await faceapi.detectSingleFace(videoRef.current, detectorOptions);
 
       getDetectorOptions().then((detectorOptions) => {
-        var animate = function () {
+        detect(detectorOptions).then((detections) => console.log('detections', detections));
+
+        const animate = function () {
           requestAnimationFrame( animate );
     
           cube.rotation.x += 0.01;
           cube.rotation.y += 0.01;
     
-          detect(detectorOptions).then((detections) => console.log('detections', detections));
+          detect(detectorOptions).then((detections) => {
+            // Detections availabe!
+          });
 
           renderer.render( scene, camera );
         };
