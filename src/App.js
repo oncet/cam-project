9cd4de6d8cadce6a99e07449da0944b9fbffea3b
oncet <camilorivera86@gmail.com>
2020-02-22 18:03:10 -0300

    Moved all filter logic to separated file

--- src/App.js
+++ src/App.js
@@ -1,74 +1,6 @@
 import React, { useRef, useEffect } from 'react';
-import * as THREE from 'three';
-import * as faceapi from 'face-api.js';
 
-const getDetectorOptions = async () => {
-  await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
-  return new faceapi.TinyFaceDetectorOptions({ inputSize: 320 });
-};
-
-const createAssets = (videoRef) => {
-  const scene = new THREE.Scene();
-
-  const renderer = new THREE.WebGLRenderer();
-
-  renderer.setSize(videoRef.current.videoWidth, videoRef.current.videoHeight);
-
-  const backgroundGeometry = new THREE.BoxGeometry(
-    videoRef.current.videoWidth,
-    videoRef.current.videoHeight,
-  );
-  const backgroundTexture = new THREE.VideoTexture(videoRef.current);
-  const backgroundMaterial = new THREE.MeshBasicMaterial({ map: backgroundTexture });
-  const background = new THREE.Mesh(backgroundGeometry, backgroundMaterial);
-
-  background.position.z = -500;
-
-  scene.add(background);
-
-  const cubeGeometry = new THREE.BoxGeometry();
-  const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-
-  cube.position.z = 5;
-
-  scene.add(cube);
-
-  const camera = new THREE.PerspectiveCamera(
-    50,
-    videoRef.current.videoWidth / videoRef.current.videoHeight,
-    0.1,
-    1000,
-  );
-
-  camera.position.z = 10;
-
-  return {
-    scene,
-    camera,
-    renderer,
-    cube,
-  };
-};
-
-const render = (assets, detectorOptions, videoRef) => {
-  const {
-    renderer,
-    scene,
-    camera,
-    cube,
-  } = assets;
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  faceapi.detectSingleFace(videoRef.current, detectorOptions).then((detections) => {
-    // Detections availabe!
-    // console.log('detections', detections);
-  });
-
-  renderer.render(scene, camera);
-};
+import videoFilter from './filters/myFirstFilter';
 
 const App = () => {
   const canvasContainerRef = useRef();
@@ -76,13 +8,13 @@ const App = () => {
 
   useEffect(() => {
     videoRef.current.onloadedmetadata = () => {
-      const assets = createAssets(videoRef);
+      const assets = videoFilter.createAssets(videoRef);
 
       canvasContainerRef.current.appendChild(assets.renderer.domElement);
 
-      getDetectorOptions().then((detectorOptions) => {
+      videoFilter.getDetectorOptions().then((detectorOptions) => {
         const animate = () => {
-          render(assets, detectorOptions, videoRef);
+          videoFilter.render(assets, detectorOptions, videoRef);
           requestAnimationFrame(() => animate());
         };
         animate();
