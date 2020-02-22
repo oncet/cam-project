ef49d0d010b7c86a70a3778f2e49a46750160b85
oncet <camilorivera86@gmail.com>
2020-02-22 16:26:41 -0300

    Fixed most eslint problems

--- src/App.js
+++ src/App.js
@@ -7,34 +7,41 @@ const App = () => {
   const videoRef = useRef();
 
   useEffect(() => {
-
     videoRef.current.onloadedmetadata = () => {
       const scene = new THREE.Scene();
-  
+
       const renderer = new THREE.WebGLRenderer();
 
-      renderer.setSize( videoRef.current.videoWidth, videoRef.current.videoHeight );
-      canvasContainerRef.current.appendChild( renderer.domElement );
-  
-      const backgroundGeometry = new THREE.BoxGeometry(videoRef.current.videoWidth, videoRef.current.videoHeight);
-      const backgroundTexture = new THREE.VideoTexture( videoRef.current );
-      const backgroundMaterial = new THREE.MeshBasicMaterial( { map: backgroundTexture } );
-      const background = new THREE.Mesh( backgroundGeometry, backgroundMaterial );
-  
-      background.position.z = -500; 
-  
-      scene.add( background );
-  
+      renderer.setSize(videoRef.current.videoWidth, videoRef.current.videoHeight);
+      canvasContainerRef.current.appendChild(renderer.domElement);
+
+      const backgroundGeometry = new THREE.BoxGeometry(
+        videoRef.current.videoWidth,
+        videoRef.current.videoHeight,
+      );
+      const backgroundTexture = new THREE.VideoTexture(videoRef.current);
+      const backgroundMaterial = new THREE.MeshBasicMaterial({ map: backgroundTexture });
+      const background = new THREE.Mesh(backgroundGeometry, backgroundMaterial);
+
+      background.position.z = -500;
+
+      scene.add(background);
+
       const cubeGeometry = new THREE.BoxGeometry();
-      const cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-      const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+      const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
       cube.position.z = 5;
-  
-      scene.add( cube );
 
-      const camera = new THREE.PerspectiveCamera( 50, videoRef.current.videoWidth/videoRef.current.videoHeight, 0.1, 1000 );
-  
+      scene.add(cube);
+
+      const camera = new THREE.PerspectiveCamera(
+        50,
+        videoRef.current.videoWidth / videoRef.current.videoHeight,
+        0.1,
+        1000,
+      );
+
       camera.position.z = 10;
 
       const getDetectorOptions = async () => {
@@ -42,24 +49,27 @@ const App = () => {
         return new faceapi.TinyFaceDetectorOptions({ inputSize: 320 });
       };
 
-      const detect = async (detectorOptions) => await faceapi.detectSingleFace(videoRef.current, detectorOptions);
+      const detect = async (detectorOptions) => (
+        faceapi.detectSingleFace(videoRef.current, detectorOptions)
+      );
 
       getDetectorOptions().then((detectorOptions) => {
         detect(detectorOptions).then((detections) => console.log('detections', detections));
 
-        const animate = function () {
-          requestAnimationFrame( animate );
-    
+        const animate = () => {
+          requestAnimationFrame(animate);
+
           cube.rotation.x += 0.01;
           cube.rotation.y += 0.01;
-    
+
           detect(detectorOptions).then((detections) => {
             // Detections availabe!
+            // console.log('detections', detections);
           });
 
-          renderer.render( scene, camera );
+          renderer.render(scene, camera);
         };
-    
+
         animate();
       });
     };
