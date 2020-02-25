bf2b62c3920a6cd166aa9559ad9e19fe32a65c34
oncet <camilorivera86@gmail.com>
2020-02-25 16:36:58 -0300

    Added filters select

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { useRef, useEffect } from 'react';
-
-import videoFilter from './filters/myFirstFilter';
+import React, { useRef, useEffect, useState } from 'react';
+import './App.css';
 
 const App = () => {
   const canvasContainerRef = useRef();
   const videoRef = useRef();
+  const [filter, setFilter] = useState('myFirstFilter');
 
   useEffect(() => {
-    videoRef.current.onloadedmetadata = () => {
+    const loadFilter = async () => {
+      const { default: videoFilter } = await import(`./filters/${filter}`); 
       const assets = videoFilter.createAssets(videoRef);
 
-      canvasContainerRef.current.appendChild(assets.renderer.domElement);
+      if (canvasContainerRef.current.firstChild) {
+        canvasContainerRef.current.replaceChild(assets.renderer.domElement, canvasContainerRef.current.firstChild);
+      } else {
+        canvasContainerRef.current.appendChild(assets.renderer.domElement);
+      }
 
       videoFilter.getDetectorOptions().then((detectorOptions) => {
         const animate = () => {
@@ -20,10 +25,15 @@ const App = () => {
         animate();
       });
     };
-  }, []);
+    loadFilter();
+  }, [filter]);
 
   return (
     <div>
+      <select onChange={(e) => setFilter(e.target.value)}>
+        <option value="myFirstFilter">My First Filter</option>
+        <option value="mySecondFilter">My Second Filter</option>
+      </select>
       <div className="canvasContainer" ref={canvasContainerRef} />
       <video muted autoPlay loop src="video.mp4" ref={videoRef} />
     </div>
