c01b66769cc8927ba2287f94ef6b7864f895cfe8
oncet <camilorivera86@gmail.com>
2020-03-04 00:07:12 -0300

    Positioning background and camera

--- src/filters/myFirstFilter.js
+++ src/filters/myFirstFilter.js
@@ -4,11 +4,11 @@ import * as faceapi from 'face-api.js';
 const createAssets = () => {
   const scene = new THREE.Scene();
 
-  const cubeGeometry = new THREE.BoxGeometry();
+  const cubeGeometry = new THREE.BoxGeometry(150, 150, 150);
   const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
   const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-  cube.position.z = 5;
+  cube.position.z = -2000;
 
   scene.add(cube);
 
@@ -32,8 +32,9 @@ const render = (assets, camera, renderer, video) => {
   cube.rotation.y += 0.01;
 
   faceapi.detectSingleFace(video, detectorOptions).then((detections) => {
-    // Detections availabe!
-    // console.log('detections', detections);
+    if (detections) {
+      // Detections availabe...
+    }
   });
 
   renderer.render(scene, camera);
