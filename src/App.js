5dcf1b69bbd61e0d7705786a695ccea65d713788
oncet <camilorivera86@gmail.com>
2020-03-07 00:38:25 -0300

    Create video using JSX

--- src/App.js
+++ src/App.js
@@ -1,75 +1,28 @@
-import React, { useRef, useEffect, useState } from 'react';
-import * as THREE from 'three';
-import * as faceapi from 'face-api.js';
+import React, { useState } from 'react';
 import './App.css';
-import helpers from './helpers.js';
 
-const loadedModels = [];
+import Filter from './Filter';
+
+const createVideo = () => (
+  <video
+    src="video.mp4"
+    muted
+    loop
+  />
+);
 
 const App = () => {
-  const canvasContainerRef = useRef();
-  const videoRef = useRef();
-  const rendererRef = useRef();
-  const animationRequestIdRef = useRef();
-  const backgroundRef = useRef();
-  const cameraRef = useRef();
   const [filter, setFilter] = useState('myFirstFilter');
-
-  useEffect(() => {
-    const loadFilter = async () => {
-      const { default: videoFilter } = await import(`./filters/${filter}`); 
-
-      if (!videoFilter.models.every(model => loadedModels.includes(model))) {
-        await Promise.all(videoFilter.models.map(async model => {
-          if (!loadedModels.includes(model)) {
-            await faceapi.nets[model].loadFromUri('/models');
-            loadedModels.push(model);
-          }
-        }));
-      }
-
-      if (!rendererRef.current) {
-        rendererRef.current = new THREE.WebGLRenderer();
-
-        videoRef.current = helpers.createVideo();
-
-        videoRef.current.onloadedmetadata  = () => {
-          rendererRef.current.setSize(videoRef.current.videoWidth, videoRef.current.videoHeight);
-          canvasContainerRef.current.appendChild(rendererRef.current.domElement);
-        }
-
-        videoRef.current.load();
-
-        await videoRef.current.play();
-
-        backgroundRef.current = helpers.createBackground(videoRef.current);
-
-        cameraRef.current = helpers.createCamera(videoRef.current);
-      }
-
-      const assets = videoFilter.createAssets();
-
-      assets.scene.add(backgroundRef.current);
-
-      cancelAnimationFrame(animationRequestIdRef.current);
-
-      const animate = () => {
-        videoFilter.render(assets, cameraRef.current, rendererRef.current, videoRef.current);
-        animationRequestIdRef.current = requestAnimationFrame(animate);
-      };
-
-      animate();
-    };
-    loadFilter();
-  }, [filter]);
-
   return (
     <div>
       <select onChange={(e) => setFilter(e.target.value)}>
         <option value="myFirstFilter">My First Filter</option>
         <option value="mySecondFilter">My Second Filter</option>
       </select>
-      <div className="canvasContainer" ref={canvasContainerRef} />
+      <Filter
+        filter={filter}
+        createVideo={createVideo}
+      />
     </div>
   );
 };
---
+++ src/Filter.js
@@ -0,0 +1,69 @@
+import React, { useRef, useEffect } from 'react';
+import * as THREE from 'three';
+import * as faceapi from 'face-api.js';
+
+import helpers from './helpers.js';
+
+const loadedModels = [];
+
+const Filter = ({ createVideo, filter }) => {
+  const animationRequestIdRef = useRef();
+  const backgroundRef = useRef();
+  const cameraRef = useRef();
+  const canvasContainerRef = useRef();
+  const rendererRef = useRef();
+  const videoRef = useRef(createVideo());
+  console.log(videoRef.current);
+  useEffect(() => {
+    const loadFilter = async () => {
+      const { default: videoFilter } = await import(`./filters/${filter}`); 
+
+      if (!videoFilter.models.every(model => loadedModels.includes(model))) {
+        await Promise.all(videoFilter.models.map(async model => {
+          if (!loadedModels.includes(model)) {
+            await faceapi.nets[model].loadFromUri('/models');
+            loadedModels.push(model);
+          }
+        }));
+      }
+
+      if (!rendererRef.current) {
+        rendererRef.current = new THREE.WebGLRenderer();
+
+        videoRef.current.onloadedmetadata = () => {
+          rendererRef.current.setSize(videoRef.current.videoWidth, videoRef.current.videoHeight);
+          canvasContainerRef.current.appendChild(rendererRef.current.domElement);
+        }
+
+        videoRef.current.load();
+
+        await videoRef.current.play();
+
+        backgroundRef.current = helpers.createBackground(videoRef);
+        cameraRef.current = helpers.createCamera(videoRef);
+      }
+
+      const assets = videoFilter.createAssets();
+
+      assets.scene.add(backgroundRef.current);
+
+      cancelAnimationFrame(animationRequestIdRef.current);
+
+      const animate = () => {
+        videoFilter.render(assets, cameraRef.current, rendererRef.current, videoRef);
+        animationRequestIdRef.current = requestAnimationFrame(animate);
+      };
+
+      animate();
+    };
+    loadFilter();
+  }, [filter]);
+
+  return (
+    <div>
+      <div className="canvasContainer" ref={canvasContainerRef} />
+    </div>
+  );
+};
+
+export default Filter;
